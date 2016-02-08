82b8132c8f59a1235c8f0f2560cb37adbc7f8c95
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-02-08 13:00:43 -0800

    Add concat task for build-js

--- gulp/build-js.js
+++ gulp/build-js.js
@@ -1,6 +1,7 @@
 module.exports = function (paths, gulp, gulpModules, environment) {
     return function() {
         return gulp.src(paths.jsDir + '/**/*.js')
+            .pipe(gulpModules.concat('boilerplate.js'))
             .pipe(environment.production ? gulpModules.uglify() : gulpModules.gutil.noop())
             .on('error', gulpModules.notify.onError("Error: <%= error.message %>"))
             .pipe(environment.production ? gulpModules.rename({extname: '.min.js'}) : gulpModules.gutil.noop())
